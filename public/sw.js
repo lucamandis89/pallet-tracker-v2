7bf57e2257da54a7c4f33356fbfc4203b029111d
lucamandis89 <lucamandis@gmail.com>
2026-02-18 10:00:54 +0100

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,24 +1,43 @@
 const CACHE_NAME = "pallet-tracker-cache-v1";
 
-const urlsToCache = [
+const FILES_TO_CACHE = [
   "/",
-    "/manifest.webmanifest",
-      "/icon-192.png",
-        "/icon-512.png"
-        ];
+  "/manifest.webmanifest",
+  "/icon-192.png",
+  "/icon-512.png"
+];
 
-        self.addEventListener("install", (event) => {
-          event.waitUntil(
-              caches.open(CACHE_NAME).then((cache) => {
-                    return cache.addAll(urlsToCache);
-                        })
-                          );
-                          });
+// Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
 
-                          self.addEventListener("fetch", (event) => {
-                            event.respondWith(
-                                caches.match(event.request).then((response) => {
-                                      return response || fetch(event.request);
-                                          })
-                                            );
-                                            });
+// Activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
